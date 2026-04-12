06d1f85bbe90fa485f309a7c3ed800e5597313f4
FAIZANxNXT <arham122ansari@gmail.com>
2026-04-12 23:56:42 +0530

    Implement user registration and login functionality

--- server.js
+++ server.js
@@ -1,12 +1,55 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
+// Fake database (temporary)
+let users = [];
+
+// Home
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, () => {
+// Register page
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "register.html"));
+});
+
+// Login page
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
+});
+
+// Register system
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  users.push({
+    username,
+    password,
+    balance: 0
+  });
+
+  res.send("Registered Successfully ✅ <br><a href='/login'>Login</a>");
+});
+
+// Login system
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (user) {
+    res.send(`Welcome ${user.username} 💰 Balance: ₹${user.balance}`);
+  } else {
+    res.send("Invalid Login ❌");
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server running...");
 });
