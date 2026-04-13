8cecb6be3d4a13432e4b0d1a30a33ee1aafe7359
FAIZANxNXT <arham122ansari@gmail.com>
2026-04-13 22:13:55 +0530

    Clean up server.js by removing unused code
    Removed unused routes and middleware from server.js.

--- server.js
+++ server.js
@@ -3,25 +3,13 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-// Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "register.html"));
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
-});
-
-// IMPORTANT PORT FIX
 const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+app.listen(PORT, () => {
+  console.log("Running...");
 });
